cb0d2003d53125fe9a7c051e9473a67dc4ffb76a
P-manBrowm <tachikawa.yuya@outlook.jp>
2022-01-25 13:23:45 +0900

    finish: 1-2-4

---
+++ src/array.ts
@@ -0,0 +1,20 @@
+export {}
+
+// 配列
+let numbers: number[] = [1, 2, 3]
+let strings: string[] = ["TypeScript", "JavaScript"]
+
+// 二次元配列
+let nijigenhairetu: number[][] = [
+  [2, 3],
+  [4, 5]
+]
+
+// 複数要素の配列（共用型）
+let hairetu: (string | number | boolean)[] = [1, "Tokyo", false]
+
+
+
+// 非推奨
+let numbers2: Array<number> = [1, 2, 3]
+let strings2: Array<string> = ["Tokyo", "Osaka", "Nagoya"]
