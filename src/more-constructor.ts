2898b9df0daa3f6408157306a6488d26a7143229
P-manBrowm <tachikawa.yuya@outlook.jp>
2022-01-30 17:14:45 +0900

    finish: 1-4-3

---
+++ src/more-constructor.ts
@@ -0,0 +1,19 @@
+export {}
+
+// class Person {
+//   public name: string
+//   private age: number
+//   constructor(name: string, age: number) {
+//     this.name = name
+//     this.age = age
+//   }
+// 上記を以下のように記述することができる
+class Person {
+  constructor(public name: string, protected age: number) {
+  }
+}
+
+const me = new Person("田中", 104)
+console.log(me)
+
+
