c1f8a50fc0240ffaf114ca8b92fb881c4dea33c9
P-manBrowm <tachikawa.yuya@outlook.jp>
2022-01-31 19:05:14 +0900

    finish: 1-4-8

---
+++ src/inheritance.ts
@@ -0,0 +1,30 @@
+export {}
+
+class Animal {
+  constructor(public name: string) {}
+  run(): string {
+    return "I can run"
+  }
+}
+
+class Lion extends Animal {
+  public speed: number
+  constructor(name: string, speed: number) {
+    super(name)
+    this.speed = speed
+  }
+  run(): string {
+    return `${super.run()} ${this.speed} km/h`
+  }
+}
+
+// let animal = new Animal()
+// console.log(animal.run())
+// // > I can run
+
+// let lion = new Lion()
+// console.log(lion.run())
+// // > I can run 80 km/h
+
+console.log(new Animal("Mickey").run())
+console.log(new Lion("Simba", 100).run())
