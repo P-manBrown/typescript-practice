10207df9e13510eea08ed6cef48c12e9f677ca1f
P-manBrowm <tachikawa.yuya@outlook.jp>
2022-02-05 14:21:51 +0900

    finish: 1-6-7

---
+++ src/pick-and-omit.ts
@@ -0,0 +1,21 @@
+export {}
+
+type DetailedProfile = {
+  name: string
+  height: number
+  weight: number
+}
+
+type SimpleProfile = Pick<DetailedProfile, "name" | "weight">
+
+type Pick<T, K extends keyof T> = {
+  [P in K]: T[P];
+};
+
+type SmallProfile = Omit<DetailedProfile, "height">
+// type Omit<T, K extends keyof any> = Pick<T, Exclude<keyof T, K>>;
+// K = height
+// keyof T = "name" | "height" | "weight"
+// Exclude<keyof T, K> = Exclude<"name" | "height" | "weight", "height"> = "name", "weight"
+type MyOmit = Pick<DetailedProfile, 'name' | 'weight'>;
+type MySmallProfile = MyOmit
