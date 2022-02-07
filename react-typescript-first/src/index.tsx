c14bc57659ebbb1f52cf752a1e38b6f4dd5cf70b
P-manBrowm <tachikawa.yuya@outlook.jp>
2022-02-07 16:34:51 +0900

    finish: 1-7-3

---
+++ react-typescript-first/src/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+// type Foo = JSX.IntrinsicAttributes
+// interface IntrinsicAttributes extends React.Attributes { }
+//  interface Attributes {
+//   key?: Key;
+// }
+// 上記の通りkeyでエラーは発生しない
+
+// index.htmlのid=rootの箇所を<App />に置き換える
+ReactDOM.render(
+  <App message="Hello, React!!" />, 
+  document.getElementById('root')
+);
