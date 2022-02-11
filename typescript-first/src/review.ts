32af04f107e1b849096f15e4297678971d07a1dd
P-manBrowm <tachikawa.yuya@outlook.jp>
2022-02-11 14:32:02 +0900

    finish: 1-review-4

--- typescript-first/src/review.ts
+++ typescript-first/src/review.ts
@@ -11,6 +11,13 @@ sayHello(firstName)
 // sayHello(age)
 // error TS2345: Argument of type 'number' is not assignable to parameter of type 'string'.
 
-const message = "hello!"
+// const message = "hello!"
 // ブロック スコープの変数 'message' を再宣言することはできません。
 
+// プリミティブ型
+let age: number = 32
+let isDone: boolean = false
+let color: string = "赤"
+
+// let mynumber: number = "300"
+// 型 'string' を型 'number' に割り当てることはできません。ts(2322)
