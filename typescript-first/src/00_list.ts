e29ee6a2e0b9ce05298c88e56e99ceb783c36aa1
P-manBrown <80106172+P-manBrown@users.noreply.github.com>
2022-02-21 10:42:25 +0900

    finish: 3.5

--- typescript-first/src/00_list.ts
+++ typescript-first/src/00_list.ts
@@ -302,3 +302,109 @@ export {}
 // オブジェクトそのものではなく、クラスやオブジェクトの一部のプロパティや
 // 関数含む一部の振る舞いを定義するのであれば、インタフェースを使用するべき
 
+
+// クラス
+// クラスに対して型を定義することができる
+// class クラス名 {
+//   フィールド1: 型1;
+//   フィールド2: 型2;
+//   ..
+// }
+
+// class Point {
+//   x: number;
+//   y: number;
+//   // 引数がない場合の初期値を指定
+//   constructor(x: number = 0, y: number = 0) {
+//     this.x = y
+//     this.y = y
+//   }
+//   // 戻り値がない関数を定義するためにvoidを指定
+//   moveX(n: number): void {
+//     this.x += n
+//   }
+//   moveY(n: number): void {
+//     this.y += n
+//   }
+// }
+
+// const point = new Point()
+// point.moveX(10)
+// console.log(`${point.x}, ${point.y}`)      // 10, 0
+
+// extendsを使用して他のクラスを継承できる
+// class Point3D extends Point {
+//   z: number;
+
+//   constructor(x: number = 0, y: number = 0, z: number = 0) {
+//     // 継承元のコンストラクタを呼び出す
+//     super(x, y)
+//     this.z = z
+//   }
+
+//    moveZ(n: number): void {
+//     this.z += n
+//    }
+// }
+
+// const point3D = new Point3D()
+// // 継承元のメソッドを呼び出せる
+// point3D.moveX(10)
+// point3D.moveZ(20)
+// console.log(`${point3D}, ${point3D.y}, ${point3D.z}`) // 10, 0, 20
+
+// インタフェースに対してimplementsを使用することでクラスに対する実装を強制可能
+
+// interface IUser {
+//   name: string;
+//   age: number;
+//   sayHello: () => string; // 引数なしで文字列を返却
+// }
+
+// class User implements IUser {
+//   name: string;
+//   age: number;
+
+//   constructor() {
+//     this.name = ""
+//     this.age = 0
+//   }
+// }
+
+// インタフェースに定義されているメソッドを実装しないとエラーになる
+// sayHello(): string {
+//   return `こんにちは、私は${this.name}、${this.age}歳です。`
+// }
+
+// const user = new User()
+// user.name = "田中"
+// user.age = 34
+
+// console.log(user.sayHello())
+
+// アクセス修飾子
+// クラスには public, private, protected がある
+// これらを扶養することでメンバやメソッドのアクセス範囲を制御できる
+// アクセス修飾子を指定しない場合はpublicとして扱われる
+
+// class BasePoint3D {
+//   public x: number;
+//   private y: number;
+//   protected z: number;
+// }
+
+// // インスタンス化を行なった場合のアクセス制御の例
+// const basePoint = new BasePoint3D()
+// basePoint.x
+// basePoint.y // privateであるためアクセス不可
+// basePoint.z // protectedのためアクセス不可
+
+// // クラスを継承した際のアクセス制御
+// class ChildPoint extends BasePoint3D {
+//   constructor() {
+//     super()
+//     this.x
+//     this.y // privateであるためアクセス不可
+//     this.z
+//   }
+// }
