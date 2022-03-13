6a3d097a612c5e69ba04b9f2eb839f5f4d66edd2
P-manBrown <80106172+P-manBrown@users.noreply.github.com>
2022-03-13 11:32:42 +0900

    finish: 5.5

--- typescript-first/src/00_list.ts
+++ typescript-first/src/00_list.ts
@@ -708,3 +708,19 @@ export {}
 
 // genderはオブジェクトのキーに存在しないためコンパイルエラー
 // const userGender = getProperty(user, "gender")
+
+
+// インデックス型
+// オブジェクトのプロパティが可変のとき、まとめて型を定義できる
+
+// type SupportVersions = {
+//   [env: number]: boolean;
+// }
+
+// stringのプロパティに定義した場合エラーが起きる
+// let versions: SupportVersions = {
+//   102: false,
+//   103: false,
+//   104: false,
+//   "v105": true, // エラー
+// }
